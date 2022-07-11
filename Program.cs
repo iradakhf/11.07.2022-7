61556a6fb237ba0e03ea20ca8f5382ebdbf3f55a
iradakhf <iradakhf@code.edu.az>
2022-07-11 15:17:32 +0400

    add all

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace ConsoleApp35
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Repeater repeater = new(4, "Irada");
+            repeater.ReturnWordRepeatedNumberTimes();
+        }
+      
+    }
+   class Repeater
+    {
+        public int NumberOfString { get; set; }
+        public string String { get; set; }
+        public Repeater(int numberOfString, string s)
+        {
+            this.NumberOfString = numberOfString;
+            this.String = s;
+        }
+        public void ReturnWordRepeatedNumberTimes()
+        {
+            for (int i = 1; i <= NumberOfString; i++)
+            {
+                Console.Write(String);
+            }
+            
+        }
+    }
+}
